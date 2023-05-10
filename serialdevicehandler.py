963c8467271dcf0b4f8728cae18f05e52f61e4f1
Salliii <99385051+Salliii@users.noreply.github.com>
2023-05-10 18:46:34 +0200

    added class variables

--- serialdevicehandler.py
+++ serialdevicehandler.py
@@ -4,8 +4,13 @@ import os
 
 class SerialDeviceHandler(object):    
     def __init__(self, port=None, baudrate=None, timeout=None) -> None:
-        
+
         self.serial_interface = serial.Serial()
         self.serial_interface.port = str( port if port != None else ("COM1" if os.name == "nt" else "/dev/tty") )
         self.serial_interface.baudrate = str( baudrate if baudrate != None else "9600" )
         self.serial_interface.timeout = float( max(timeout, 0.2) if timeout != None else 0.2 )
+
+        self.buffer = str()
+        self.is_open = bool()
+        self.is_executing = bool()
+        self.eol_delimiter = str("\n")
